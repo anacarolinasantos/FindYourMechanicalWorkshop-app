1c6257b5cdc70e2a1293812162ae68d4a5c7d7fb
Ana Carolina dos Santos Silva <ana.carolinasantos@icloud.com>
2019-07-31 17:35:38 -0300

    Add API classes

---
+++ FindYourMechanicalWorkshop/Source/Services/PersistencyManager.swift
@@ -0,0 +1,17 @@
+//
+//  PersistencyManager.swift
+//  FindYourMechanicalWorkshop
+//
+//  Created by Ana Carolina Silva on 31/07/19.
+//  Copyright © 2019 Ana Carolina Silva. All rights reserved.
+//
+
+import Foundation
+
+final class PersistencyManager {
+    
+    // MARK: - Initializer
+    
+    // MARK: - Public Methods
+    
+}
